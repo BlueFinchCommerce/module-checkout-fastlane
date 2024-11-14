b3fc013b51d6594ea62352c423a478b192eda906
Alla Nehaenko <allanehaenko@gmail.com>
2024-11-14 12:57:22 +0000

    added fixes for fastlane module extension after BC changes

--- view/frontend/web/js/checkout/dist/components/Watermark.js
+++ view/frontend/web/js/checkout/dist/components/Watermark.js
@@ -1,4 +1,4 @@
-import { m as mapState, a as mapActions, u as useFastlaneStore, c as createElementBlock, d as createCommentVNode, o as openBlock, e as createBaseVNode } from '../FastlaneStore-DtEpjRQt.js';
+import { m as mapState, a as mapActions, u as useFastlaneStore, c as createElementBlock, e as createBaseVNode, d as createCommentVNode, o as openBlock } from '../FastlaneStore-D7NEX6o0.js';
 
 var script = {
   name: 'FastlaneWatermark',
@@ -26,17 +26,19 @@ var script = {
 };
 
 const _hoisted_1 = ["id"];
-const _hoisted_2 = /*#__PURE__*/createBaseVNode("img", { src: "https://www.paypalobjects.com/fastlane-v1/assets/fastlane-with-tooltip_en_sm_light.0808.svg" }, null, -1 /* HOISTED */);
-const _hoisted_3 = [
-  _hoisted_2
-];
+const _hoisted_2 = ["alt"];
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   return (_ctx.config.paypal_fastlane_is_active)
     ? (openBlock(), createElementBlock("div", {
         key: 0,
         id: $data.id
-      }, [..._hoisted_3], 8 /* PROPS */, _hoisted_1))
+      }, [
+        createBaseVNode("img", {
+          alt: $data.id,
+          src: "https://www.paypalobjects.com/fastlane-v1/assets/fastlane-with-tooltip_en_sm_light.0808.svg"
+        }, null, 8 /* PROPS */, _hoisted_2)
+      ], 8 /* PROPS */, _hoisted_1))
     : createCommentVNode("v-if", true)
 }
 
